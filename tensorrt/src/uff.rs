dbb8ec8f6f8b276799e412dddfd1fa293b67ed6c
Mason Stallmo <masonstallmo@gmail.com>
2020-04-09 22:25:17 -0700

    Add some custom error handling to regsiter input and output for uff parsing.

--- tensorrt/src/uff.rs
+++ tensorrt/src/uff.rs
@@ -1,6 +1,8 @@
 use crate::network::Network;
+use std::error;
 use std::ffi::CString;
-use std::io::{Error, ErrorKind};
+use std::fmt::Formatter;
+use std::io;
 use std::path::Path;
 use tensorrt_sys::{
     uffparser_create_uff_parser, uffparser_destroy_uff_parser, uffparser_parse,
@@ -19,35 +21,54 @@ impl UffParser {
         }
     }
 
-    pub fn register_input(&self, input_name: &str) {
+    pub fn register_input(&self, input_name: &str) -> Result<(), UFFRegistrationError> {
         let mut d_vec = vec![3, 256, 256];
+
+        // Could be a better way to model this in Rust and pass it to C.
+        // Value represents what type each of dimensions above are which is not well communicated
+        // by the [1, 0, 0] that currently exists.
         let mut type_vec = vec![1, 0, 0];
         let dims = Dims {
             nbDims: 3,
             d: d_vec.as_mut_ptr(),
             type_: type_vec.as_mut_ptr(),
         };
-        unsafe {
+        let res = unsafe {
             uffparser_register_input(
                 self.internal_uffparser,
                 CString::new(input_name).unwrap().as_ptr(),
                 dims,
             )
         };
+
+        if res {
+            Ok(())
+        } else {
+            Err(UFFRegistrationError::new("Input Registration Failed"))
+        }
     }
 
-    pub fn register_output(&self, output_name: &str) {
-        unsafe {
+    pub fn register_output(&self, output_name: &str) -> Result<(), UFFRegistrationError> {
+        let res = unsafe {
             uffparser_register_output(
                 self.internal_uffparser,
                 CString::new(output_name).unwrap().as_ptr(),
             )
         };
+
+        if res {
+            Ok(())
+        } else {
+            Err(UFFRegistrationError::new("Output Registration Failed"))
+        }
     }
 
     pub fn parse(&self, file_path: &Path, network: &Network) -> Result<(), std::io::Error> {
         if !file_path.exists() {
-            return Err(Error::new(ErrorKind::NotFound, "UFF file does not exist"));
+            return Err(io::Error::new(
+                io::ErrorKind::NotFound,
+                "UFF file does not exist",
+            ));
         }
 
         unsafe {
@@ -66,3 +87,24 @@ impl Drop for UffParser {
         unsafe { uffparser_destroy_uff_parser(self.internal_uffparser) };
     }
 }
+
+#[derive(Debug, Clone)]
+pub struct UFFRegistrationError {
+    message: String,
+}
+
+impl UFFRegistrationError {
+    pub fn new(message: &str) -> Self {
+        UFFRegistrationError {
+            message: message.to_string(),
+        }
+    }
+}
+
+impl std::fmt::Display for UFFRegistrationError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.message)
+    }
+}
+
+impl error::Error for UFFRegistrationError {}

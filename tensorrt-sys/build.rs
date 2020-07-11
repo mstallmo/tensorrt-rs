5a534bb26841392f7d50dfd9da6e0e3025b74927
Jan Van Sweevelt <vansweej@gmail.com>
2020-07-11 22:05:58 +0200

    cudart

--- tensorrt-sys/build.rs
+++ tensorrt-sys/build.rs
@@ -8,10 +8,10 @@ fn main() {
     println!("cargo:rustc-link-search=native={}", dst.display());
     println!("cargo:rustc-link-lib=static=trt-sys");
 //    println!("cargo:rustc-flags=-l dylib=stdc++");
-    println!("cargo:rustc-flags=-L D:\\projects\\TensorRT-7.1.3.4\\lib");
+    println!("cargo:rustc-link-search=D:/projects/TensorRT-7.1.3.4/lib");
     println!("cargo:rustc-flags=-l dylib=nvinfer");
     println!("cargo:rustc-flags=-l dylib=nvparsers");
 //    println!("cargo:rustc-flags=-L /usr/local/cuda/lib64");
-    println!(r#"cargo:rustc-link-search=-L C:/{}/{}/CUDA/v11.0/lib/x64"#, "Program Files", "NVIDIA GPU Computing Toolkit");
+    println!(r#"cargo:rustc-link-search=C:/{}/{}/CUDA/v11.0/lib/x64"#, "Program Files", "NVIDIA GPU Computing Toolkit");
     println!("cargo:rustc-flags=-l dylib=cudart");
 }

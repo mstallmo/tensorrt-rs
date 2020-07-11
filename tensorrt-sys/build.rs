ec91790249b53a5cd1f87f7546c7fcf99a72831e
Jan Van Sweevelt <vansweej@gmail.com>
2020-07-11 21:53:08 +0200

    added link path to tensorrt libs

--- tensorrt-sys/build.rs
+++ tensorrt-sys/build.rs
@@ -8,6 +8,7 @@ fn main() {
     println!("cargo:rustc-link-search=native={}", dst.display());
     println!("cargo:rustc-link-lib=static=trt-sys");
 //    println!("cargo:rustc-flags=-l dylib=stdc++");
+    println!("cargo:rustc-flags=-L D:/projects/TensorRT-7.1.3.4/lib");
     println!("cargo:rustc-flags=-l dylib=nvinfer");
     println!("cargo:rustc-flags=-l dylib=nvparsers");
 //    println!("cargo:rustc-flags=-L /usr/local/cuda/lib64");

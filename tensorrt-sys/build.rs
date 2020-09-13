8b55767103d95b12072c6d6c07f3e9ec2e63d01c
Mason Stallmo <mason@kaliberlabs.com>
2020-09-13 11:41:23 -0700

    Add feature for enabling specific TensorRT version in tensorrt-sys

--- tensorrt-sys/build.rs
+++ tensorrt-sys/build.rs
@@ -1,13 +1,27 @@
 use cmake::Config;
+use std::env;
 
+//TODO: Finish this linker path and implement for 7.1.0
+// Figure out why the ldd for the example executable is still finding the nvinfer at the default
+// location instead of our installed location. Potentially look into making the trt-sys library a
+// dynamic library instead of static to handle the linking there rather than dealing with it on the
+// Rust side.
 fn main() {
-    let dst = Config::new("trt-sys").build();
-
-    println!("cargo:rustc-link-search=native={}", dst.display());
+    let mut config = Config::new("trt-sys");
     println!("cargo:rustc-link-lib=static=trt-sys");
     println!("cargo:rustc-flags=-l dylib=stdc++");
-    println!("cargo:rustc-flags=-l dylib=nvinfer");
-    println!("cargo:rustc-flags=-l dylib=nvparsers");
-    println!("cargo:rustc-flags=-L /usr/local/cuda/lib64");
-    println!("cargo:rustc-flags=-l dylib=cudart");
+
+    #[cfg(feature = "trt-515")]
+    {
+        let dst = config.define("TRT_VERSION", "5.1.5").build();
+        let curr_dir = env::current_dir().unwrap();
+        println!("current directory: {}", curr_dir.display());
+        println!("library directory: {}/trt-sys/libs/TensorRT-5.1.5.0/lib", curr_dir.display());
+        println!("cargo:rustc-link-search=native={}", dst.display());
+        println!("cargo:rustc-flags=-L {}/trt-sys/libs/TensorRT-5.1.5.0/lib", curr_dir.display());
+        println!("cargo:rustc-flags=-l dylib=nvinfer");
+        println!("cargo:rustc-flags=-l dylib=nvparsers");
+        println!("cargo:rustc-flags=-L /usr/local/cuda-10.1/lib64");
+        println!("cargo:rustc-flags=-l dylib=cudart");
+    }
 }
